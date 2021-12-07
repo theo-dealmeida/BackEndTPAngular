42fa79aae191dd55cb0a905f0a955e6c2c2e1d61
micbuffa <micbuffa@gmail.com>
2021-12-07 12:14:08 +0100

    First commit

---
+++ model/assignment.js
@@ -0,0 +1,20 @@
+let mongoose = require('mongoose');
+var aggregatePaginate = require("mongoose-aggregate-paginate-v2");
+
+let Schema = mongoose.Schema;
+
+let AssignmentSchema = Schema({
+    id: Number,
+    dateDeRendu: Date,
+    nom: String,
+    rendu: Boolean
+});
+
+AssignmentSchema.plugin(aggregatePaginate);
+
+// C'est à travers ce modèle Mongoose qu'on pourra faire le CRUD
+// le premier paramètre est la "collection" mongoDB
+// ATTENTION, y'a du matching entre la chaine passée
+// et le vrai nom de la collection (en gros, ça cherche la
+// collection mongoDB qui est "la plus proche")
+module.exports = mongoose.model('assignments', AssignmentSchema);
